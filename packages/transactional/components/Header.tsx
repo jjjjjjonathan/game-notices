5416fa3d1d14c241ade1b0db6a761890a4630534
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2024-05-11 07:55:42 -0400

    Update header for email

--- packages/transactional/components/Header.tsx
+++ packages/transactional/components/Header.tsx
@@ -1,25 +1,24 @@
-import { Section, Row, Column, Img, Text } from 'jsx-email';
+import { Section, Row, Column, Img, Text, Heading } from 'jsx-email';
 
-type HeaderProps = {
+export type HeaderProps = {
   leagueLogo: string;
   competitionName: string;
   dateTime: string;
 };
 
-export const Header = ({
-  leagueLogo,
-  competitionName,
-  dateTime,
-}: HeaderProps) => (
+const Header = ({ leagueLogo, competitionName, dateTime }: HeaderProps) => (
   <Section className='mx-auto w-full'>
     <Row>
-      <Column>
-        <Img src={leagueLogo} width='75' />
+      <Column className='text-left'>
+        <Heading as='h1'>Game Notice</Heading>
+        <Text>{competitionName} | matchId</Text>
+        <Text>{dateTime} | location</Text>
       </Column>
       <Column>
-        <Text className='text-right'>{competitionName}</Text>
-        <Text className='text-right'>{dateTime}</Text>
+        <Img src={leagueLogo} width='100' />
       </Column>
     </Row>
   </Section>
 );
+
+export default Header;
--- packages/transactional/templates/email.tsx
+++ packages/transactional/templates/email.tsx
@@ -1,18 +1,32 @@
-import { Tailwind, Html, Body, Container } from 'jsx-email';
-import { Header } from '../components/Header';
+import { Tailwind, Html, Body, Container, Hr } from 'jsx-email';
+import Header, { type HeaderProps } from '../components/Header';
 
-export const Template = () => (
+type EmailProps = {
+  headerProps: HeaderProps;
+};
+
+const headerProps = {
+  leagueLogo:
+    'https://mqraydcreimschkgouyk.supabase.co/storage/v1/object/public/league-logos/m-premier.png',
+  competitionName: 'League1 ON Premier Division',
+  dateTime: 'February 24, 2024',
+} satisfies HeaderProps;
+
+export const PreviewProps = {
+  headerProps,
+};
+
+export const Template = ({ headerProps }: EmailProps) => (
   <Tailwind production={true}>
     <Html lang='en'>
       <Body className='bg-white'>
         <Container>
           <Header
-            leagueLogo={
-              'https://mqraydcreimschkgouyk.supabase.co/storage/v1/object/public/league-logos/m-premier.png'
-            }
-            competitionName={'League1 ON Premier Division'}
-            dateTime={'February 4, 2024'}
+            leagueLogo={headerProps.leagueLogo}
+            competitionName={headerProps.competitionName}
+            dateTime={headerProps.dateTime}
           />
+          <Hr />
         </Container>
       </Body>
     </Html>
