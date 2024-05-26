dbdee438a4fb2a9c276c17fd9c3abb9b9aff25b8
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2024-05-26 14:44:45 -0400

    Set up tRPC

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,10 +1,20 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import { createTRPCRouter, createContext } from './trpc';
+import * as trpcExpress from '@trpc/server/adapters/express';
 
 dotenv.config();
 
+export const appRouter = createTRPCRouter({});
+export type AppRouter = typeof appRouter;
+
 const app: Express = express();
 const port = process.env.PORT || 3001;
+
+app.use(
+  '/trpc/',
+  trpcExpress.createExpressMiddleware({ router: appRouter, createContext }),
+);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello world');
