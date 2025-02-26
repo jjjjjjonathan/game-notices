af206425d6c1feeaf02c03c2686620773a9eca6d
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2025-02-26 16:00:35 -0500

    Re-enable match table pulled from COMET

--- apps/client/src/components/day-selector.tsx
+++ apps/client/src/components/day-selector.tsx
@@ -1,20 +1,20 @@
-// import { Calendar } from '@/components/ui/calendar';
-// import MatchTable from '@/components/match-table';
+import { Calendar } from '@/components/ui/calendar';
+import MatchTable from '@/components/match-table';
 import { useState } from 'react';
-// import { addDay, dayStart, format } from '@formkit/tempo';
+import { addDay, dayStart, format } from '@formkit/tempo';
 import { trpc } from '@/utils/trpc';
 import { Button } from './ui/button';
 
 const DaySelector = () => {
-  // const [date, setDate] = useState<Date | undefined>(
-  //   dayStart(addDay(new Date(), 2)),
-  // );
+  const [date, setDate] = useState<Date | undefined>(
+    dayStart(addDay(new Date(), 2)),
+  );
 
   const [signedUrl, setSignedUrl] = useState('');
 
-  // const { data, isSuccess } = trpc.comet.getMatches.useQuery({
-  //   date: format(date || new Date(), 'YYYYMMDD', 'en'),
-  // });
+  const { data, isSuccess } = trpc.comet.getMatches.useQuery({
+    date: format(date || new Date(), 'YYYYMMDD', 'en'),
+  });
 
   const { mutate } = trpc.storage.getSignedUrl.useMutation({
     onSuccess: (data) => {
@@ -24,14 +24,16 @@ const DaySelector = () => {
 
   return (
     <>
-      {/* <Calendar
+      <Calendar
         mode='single'
         selected={date}
         onSelect={setDate}
         className='rounded-md border'
       />
-      {isSuccess && <MatchTable data={data} />} */}
-      <Button onClick={() => mutate()}>Test signedUrl</Button>
+      {isSuccess && <MatchTable data={data} />}
+      <Button onClick={() => mutate({ filePath: 'match-notices/test.pdf' })}>
+        Test signedUrl
+      </Button>
       <p>
         Signed URL:{' '}
         <span>
--- apps/server/src/routers/storage.ts
+++ apps/server/src/routers/storage.ts
@@ -1,15 +1,32 @@
+import { z } from 'zod';
 import { createTRPCRouter, publicProcedure } from '../trpc';
 
-export const storageRouter = createTRPCRouter({
-  getSignedUrl: publicProcedure.mutation(async ({ ctx }) => {
-    const { data, error } = await ctx.supabase.storage
-      .from('pdf-notices')
-      .createSignedUrl('match-notices/test.pdf', 600);
+const BUCKET = 'pdf-notices';
 
-    if (data) {
-      return data.signedUrl;
-    } else {
-      return error.message;
-    }
-  }),
+export const storageRouter = createTRPCRouter({
+  storeMatchNotice: publicProcedure
+    .input(
+      z.object({
+        id: z.number(),
+      }),
+    )
+    .mutation(async ({ input, ctx }) => {}),
+
+  getSignedUrl: publicProcedure
+    .input(
+      z.object({
+        filePath: z.string().startsWith('match-notices/'),
+      }),
+    )
+    .mutation(async ({ input, ctx }) => {
+      const { data, error } = await ctx.supabase.storage
+        .from(BUCKET)
+        .createSignedUrl(input.filePath, 60);
+
+      if (data) {
+        return data.signedUrl;
+      } else {
+        return error.message;
+      }
+    }),
 });
